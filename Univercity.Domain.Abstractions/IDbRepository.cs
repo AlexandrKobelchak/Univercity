6e209f38605a6067f28f8426393a9ac917f92b50
AlexandrKobelchak <kobelchak@gmail.com>
2019-02-08 12:35:35 +0200

    Add generic async repository

--- Univercity.Domain.Abstractions/IDbRepository.cs
+++ Univercity.Domain.Abstractions/IDbRepository.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Univercity.Domain
 {
     public interface IDbRepository<T>
         where T:class, IDbEntity
     {
-        bool AddItem(T item);
-        bool AddItems(IEnumerable<T> items);
-        IQueryable<T> AllItems { get; }
-        bool ChangeItem(T item);
-        bool DeleteItem(Guid id);
-        T GetItem(Guid id);
-        bool SaveChanges();
+        IQueryable<T> AllItems { get; }        
+        DbContext Context { get; }
+        Task<List<T>> ToListAsync();
+        Task<int> AddItemAsync(T item);
+        Task<int> AddItemsAsync(IEnumerable<T> items);        
+        Task<bool> ChangeItemAsync(T item);
+        Task<bool> DeleteItemAsync(Guid id);
+        Task<T> GetItemAsync(Guid id);
+        Task<int> SaveChangesAsync();
     }
 }
